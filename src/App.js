36c29ed2539eacd1fd3edb74cea741f9746f2c26
Pavithra <polurupavithra@gmail.com>
2021-02-18 17:22:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useQueryParams, { useQueryParam } from "./useQueryParams";
 
 function App() {
+  const results = useQueryParams();
+  const result = useQueryParam("name");
+  console.log("results ", results);
+  console.log("result ", result);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>See browser console</h1>
     </div>
   );
 }
---
+++ src/useQueryParams.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function useQueryParams() {
+  const oldUrl = window.location.search;
+  let url = oldUrl.substr(1);
+
+  let params = [];
+
+  while (url.length > 0 && url.indexOf("=") != -1) {
+    const keyLastIndex = url.indexOf("=");
+    const key = url.substring(0, keyLastIndex);
+    let value;
+    const andIndex = url.indexOf("&");
+    if (andIndex != -1) {
+      value = url.substring(keyLastIndex + 1, andIndex);
+      url = url.substring(andIndex + 1);
+    } else {
+      value = url.substring(keyLastIndex + 1);
+      url = "";
+    }
+    params.push({ [key]: value });
+  }
+  return params;
+}
+
+export function useQueryParam(queryParam) {
+  const params = useQueryParams();
+  let returnValue;
+  params.map((param) => {
+    for (const [key, value] of Object.entries(param)) {
+      if (`${key}` === queryParam) {
+        returnValue = `${value}`;
+      }
+    }
+  });
+  return returnValue;
+}
+
+export default useQueryParams;
