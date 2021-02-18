3e5f774b348ea641921169aa38ce3f2bffa0da6c
Pavithra <polurupavithra@gmail.com>
2021-02-18 18:36:05 +0530

    publish folder commit

---
+++ publish_files/README.md
@@ -0,0 +1,20 @@
+# USE-QUERY-PARAMS
+
+React hook to get the all query params from the url or to get he value of a particular key.
+
+## Installation:
+
+```
+
+npm i use-params-query -s
+
+```
+
+## Basic Usage:
+
+```
+
+const params = useQueryParams();
+const param = useQueryParam("name");
+
+```
---
+++ publish_files/useQueryParams.js
@@ -0,0 +1,39 @@
+function useQueryParams() {
+  const oldUrl = window.location.search;
+  let url = oldUrl.substr(1);
+
+  let params = [];
+
+  while (url.length > 0 && url.indexOf("=") != -1) {
+    const keyLastIndex = url.indexOf("=");
+    const key = url.substring(0, keyLastIndex);
+    let value;
+    const andIndex = url.indexOf("&");
+    if (andIndex != -1) {
+      value = url.substring(keyLastIndex + 1, andIndex);
+      url = url.substring(andIndex + 1);
+    } else {
+      value = url.substring(keyLastIndex + 1);
+      url = "";
+    }
+    params.push({ [key]: value });
+  }
+  return params;
+}
+
+export function useQueryParam(queryParam) {
+  const params = useQueryParams();
+  let returnValue;
+  params.map((param) => {
+    for (const [key, value] of Object.entries(param)) {
+      if (`${key}` === queryParam) {
+        returnValue = `${value}`;
+      }
+    }
+  });
+  return returnValue
+    ? returnValue
+    : console.error("Query Param is not prsent.");
+}
+
+export default useQueryParams;
